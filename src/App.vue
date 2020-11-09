d23c7fbd23853d37fb803b7def42355ac606a483
amirulhilmyz <amirulhilmy8862@gmail.com>
2020-11-09 18:22:42 +0800

    sign in & sign up. +button login gi ke sign in pastu lupa dah haha

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,20 @@
 <template>
-  <div>
+<div>
     <v-app>
-      <v-app-bar app color="green" dark>
-        <div class="d-flex align-center">
-          <v-img
-            alt="Vuetify Logo"
-            class="shrink mr-2"
-            contain
-            src="./assets/appointment-image.jpg"
-            transition="scale-transition"
-            width="40"
-          />
-          <v-img
-            alt="Vuetify Name"
-            class="shrink mt-1 hidden-sm-and-down"
-            contain
-            min-width="100"
-            src="assets/appointment-image.jpg"
-            width="100"
-          />
-          <v-btn target="_blank" text>
-            <router-link tag="span" to="/makeAppointment">
-              <span class="mr-2">Make an Appointment</span>
-            </router-link>
-          </v-btn>
-        </div>
+        <v-app-bar app color="green" dark>
+            <div class="d-flex align-center">
+                <v-img alt="Vuetify Logo" class="shrink mr-2" contain src="./assets/appointment-image.jpg" transition="scale-transition" width="40" />
+                <v-img alt="Vuetify Name" class="shrink mt-1 hidden-sm-and-down" contain min-width="100" src="assets/appointment-image.jpg" width="100" />
+                <v-btn target="_blank" text>
+                    <router-link tag="span" to="/makeAppointment">
+                        <span class="mr-2">Make an Appointment</span>
+                    </router-link>
+                </v-btn>
+            </div>
 
-        <v-spacer></v-spacer>
+            <v-spacer></v-spacer>
 
-        <!-- <v-btn
+            <!-- <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
         text
@@ -37,42 +23,39 @@
         <v-icon>mdi-open-in-new</v-icon>
       </v-btn> -->
 
-        <v-btn target="_blank" text>
-          <router-link tag="span" to="/admin">
-            <!-- <v-icon>mdi-open-in-new</v-icon> -->
-            <span class="mr-2">Login</span>
-          </router-link>
-        </v-btn>
-        <router-link tag="span" to="/userProfile">
-          <v-avatar>
-            <img
-              src="https://cdn.vuetifyjs.com/images/john.jpg"
-              alt="John"
-              href="/userProfile"
-            />
-          </v-avatar>
-        </router-link>
-      </v-app-bar>
+            <v-btn target="_blank" text>
+                <router-link tag="span" to="/Login">
 
-      <v-main>
-        <router-view></router-view>
-      </v-main>
+                    <span class="mr-2">Login</span>
+                </router-link>
+            </v-btn>
+
+            <router-link tag="span" to="/userProfile">
+                <v-avatar>
+                    <img src="https://cdn.vuetifyjs.com/images/john.jpg" alt="John" href="/userProfile" />
+                </v-avatar>
+            </router-link>
+        </v-app-bar>
+
+        <v-main>
+            <router-view></router-view>
+        </v-main>
     </v-app>
-  </div>
+</div>
 </template>
 
 <script>
 export default {
-  name: "App",
+    name: "App",
 
-  data: () => ({
-    //
-  }),
+    data: () => ({
+        //
+    }),
 };
 </script>
 
 <style>
 li {
-  text-decoration: none;
+    text-decoration: none;
 }
 </style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,13 @@ import Vuetify from 'vuetify/lib';
 Vue.use(Vuetify);
 
 export default new Vuetify({
-});
+    theme:{
+    
+    light: {
+        background: '#292930',
+      },
+    },
+      light: true,
+}
+      
+);
